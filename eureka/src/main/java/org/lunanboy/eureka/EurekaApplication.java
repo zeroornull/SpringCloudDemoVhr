ec5a83c38aef898506de972932b7f16e8dc31c4f
zeroornull <xxpfantastic@yahoo.com>
2021-03-17 10:35:17 +0800

    firsit commit

---
+++ eureka/src/main/java/org/lunanboy/eureka/EurekaApplication.java
@@ -0,0 +1,15 @@
+package org.lunanboy.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer//表示开启Eureka功能
+public class EurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaApplication.class, args);
+	}
+
+}
